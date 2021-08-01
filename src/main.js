a625051259411443052d285ac394ec655276b5d0
ParamagicDev <konnor5456@gmail.com>
2021-08-01 00:12:44 -0400

    remove stuff for base path

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import "@shoelace-style/shoelace/dist/themes/base.css"
 import "@shoelace-style/shoelace/dist/components/icon/icon"
 import "@shoelace-style/shoelace/dist/components/icon-button/icon-button"
 
-// import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
-// import path from "path"
-// setBasePath('../../node_modules/@shoelace-style/shoelace/dist/assets/icons/');
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// Uncomment this if you want to use rollup-plugin-copy for icons instead of the package.json script
-// import * as path from "path"
-// import copy from "rollup-plugin-copy"
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  // If you would prefer not to use the script provided in package.json, uncomment below
-  // build: {
-  //   rollupOptions: {
-  //     plugins: [
-  //       copy({
-  //         targets: [
-  //           {
-  //             src: path.resolve(__dirname, 'node_modules/@shoelace-style/shoelace/dist/assets/icons'),
-  //             dest: path.resolve(__dirname, 'dist/assets')
-  //           }
-  //         ],
-  //         // https://github.com/vitejs/vite/issues/1231
-  //         hook: 'buildWrite',
-  //       })
-  //     ]
-  //   }
-  // },
   plugins: [
     vue({
       template: {
